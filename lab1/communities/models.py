1ddcca85b11125e37dc2891ad1fb58f0562e9af0
ST3606 <ST3606@TE.local>
2024-12-09 10:11:15 +0600

    {Lab2}

--- lab1/communities/models.py
+++ lab1/communities/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
+class Community(models.Model):
+    name = models.TextField(max_length=75)
+    description = models.TextField(max_length=150)
+    slug = models.SlugField()
+    date = models.DateTimeField(auto_now_add=True)
+    free = models.BooleanField()
+
+    def __str__(self):
+        return self.name
+
 # Create your models here.
