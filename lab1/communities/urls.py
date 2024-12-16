95ace6d4037cd5b73eb9b2d59d2e8688561a322a
ST3503 <ST3503@TE.local>
2024-12-16 16:26:50 +0600

    2 лаба

--- lab1/communities/urls.py
+++ lab1/communities/urls.py
@@ -2,7 +2,9 @@ from django.contrib import admin
 from django.urls import path
 from . import views
 
+app_name = 'communities'
+
 urlpatterns = [
-    path('', views.communities, name="communities")
-    path('<slug:slug>', views.community_page, name="community" )
+    path('', views.communities, name="list"),
+    path('<slug:slug>', views.community_page, name="page" )
 ]
