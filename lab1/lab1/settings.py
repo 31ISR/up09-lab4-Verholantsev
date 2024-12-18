c49160ac4602aed2776641b7f300bba6afae5f8d
zapsicakka@gufum.com <zapsicakka@gufum.com>
2024-12-18 17:19:35 +0600

    Финишная 3 лаба почти

--- lab1/lab1/settings.py
+++ lab1/lab1/settings.py
@@ -25,9 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-h4%$hmwm2@$@!mw8oo@v)^si+n0m($97*l+7i0jfmc3g&g2_w$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["localhost","127.0.0.1"]
 
 
 # Application definition
